bf652d0ae0af5fbb3acdbe56b1b7861f3e4431ac
Vanisri09876 <114634388+Vanisri09876@users.noreply.github.com>
2023-07-20 14:27:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import AskedQuestions from "./components/AskedQuestions"
+import CompanyCarousel from "./components/CompanyCarousel"
+import Contact from "./components/Contact"
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import GetStarted from "./components/GetStarted"
+import MainContent from "./components/MainContent"
+import NavBar from "./components/NavBar"
+import Sections from "./components/Sections"
+import TrustCounts from "./components/TrustCounts"
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <div className="mt-32">
+      <MainContent />
+      <CompanyCarousel />
+      <TrustCounts />
+      <Sections />
+      <Features />
+      <GetStarted />
+      <Contact />
+      <AskedQuestions />
+      <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default App
